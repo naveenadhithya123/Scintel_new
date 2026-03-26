e1518f76b8716db59d312f2dd88910edf5491977
SanthoshKumarM-techie <santhoshkumarm.designer@gmail.com>
2026-03-26 11:44:47 +0530

    Deletre logic added for glories

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,15 +1,9 @@
 import { v2 as cloudinary } from "cloudinary";
 
-const { cloud_name, api_key, api_secret } = process.env;
-
-if (!cloud_name || !api_key || !api_secret) {
-    throw new Error("Missing Cloudinary environment variables");
-}
-
 cloudinary.config({
-    cloud_name,
-    api_key,
-    api_secret
+    cloud_name: "dknfckzpe",
+    api_key: "665722189225344",
+    api_secret: "lLwtuCOTWYREBVD2lai-78RAh9s"
 });
 
 export default cloudinary;
